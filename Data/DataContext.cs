258556e237900b74e44751915584bd62824ffb0f
ashishvegan <ashishvegan@gmail.com>
2023-02-02 14:33:09 +0530

    EoS 32

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using ApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ApiProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
